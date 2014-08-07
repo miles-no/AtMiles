7e38cdf59afabd597c21c39a92e6d37e08c033e3
sedvardsen <sedvardsen@gmail.com>
2014-08-07 15:07:26 +0200

    Added endpoints

---
+++ source/Contact.Backend/Models/Api/ResponseBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Contact.Backend.Models.Api
+{
+    public class ResponseBase
+    {
+        public string Id { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class AsyncResponseBase : ResponseBase
+    {
+        public string Url { get; set; }
+    }
+}
