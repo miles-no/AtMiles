dbcf6d0b277080c0bfaea7755c5ad54e1cbc89ad
Roy Veshovda <roy@veshovda.no>
2014-08-11 15:09:25 +0200

    CommandSender in RabbitMq

---
+++ source/Contact.Infrastructure/RabbitMqCommandSender.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contact.Domain;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Contact.Infrastructure
+{
+    public class RabbitMqCommandSender : ICommandSender, IDisposable
+    {
+        private readonly string _hostName;
+        private readonly string _username;
+        private readonly string _password;
+        private readonly string _exchangeName;
+        private ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _model;
+        private readonly ushort _heartbeatInterval;
+        private readonly bool _useSsl;
+
+        public RabbitMqCommandSender(string hostName, string username, string password, string exchangeName, bool useSsl = false)
+        {
+            _hostName = hostName;
+            _username = username;
+            _password = password;
+            _exchangeName = exchangeName;
+            _useSsl = useSsl;
+            _heartbeatInterval = 30;
+
+            //TODO: Initialize connection
+        }
+
+        public void Dispose()
+        {
+            Disconnect();
+        }
+
+        public bool Send<T>(T command) where T : Command
+        {
+            if (!IsConnected())
+            {
+                Connect();
+            }
+            if (IsConnected())
+            {   
+                {
+                    IBasicProperties properties = _model.CreateBasicProperties();
+                    properties.SetPersistent(true);
+                    properties.Headers = new Dictionary<string, object>
+                    {
+                        {"type", command.GetType().Name},
+                        {"correlationid", command.CorrelationId}
+                    };
+                    string routingKey = GetRoutingKey();
+                    byte[] body = Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(command));
+                    try
+                    {
+                        _model.BasicPublish(_exchangeName, routingKey, false, true, properties, body);
+                        return true;
+                    }
+                    catch (Exception error)
+                    {
+                        //TODO: Log exception
+                    }
+                }
+            }
+            //TODO: Log error
+            return false;
+            
+        }
+
+        private string GetRoutingKey()
+        {
+            return "";
+        }
+
+        private bool Connect()
+        {
+            if (_useSsl)
+            {
+                var ssl = new SslOption();
+                ssl.Enabled = true;
+                ssl.ServerName = _hostName;
+
+                _connectionFactory = new ConnectionFactory
+                {
+                    HostName = _hostName,
+                    UserName = _username,
+                    Password = _password,
+                    RequestedHeartbeat = _heartbeatInterval,
+                    Ssl = ssl
+                };
+            }
+            else
+            {
+                _connectionFactory = new ConnectionFactory
+                {
+                    HostName = _hostName,
+                    UserName = _username,
+                    Password = _password,
+                    RequestedHeartbeat = _heartbeatInterval
+                };
+            }
+            try
+            {
+                _connection = _connectionFactory.CreateConnection();
+
+                if (_connection != null && _connection.IsOpen)
+                {
+                    _model = _connection.CreateModel();
+                    return _model != null && _model.IsOpen;
+                }
+            }
+            catch (Exception)
+            { }
+            return false;  // Failed to create connection
+        }
+
+        private void Disconnect()
+        {
+            if (_model != null)
+            {
+                lock (_model)
+                {
+                    _model.Close(200, "Goodbye");
+                    _model.Dispose();
+                }
+                _model = null;
+            }
+
+            if (_connection != null)
+            {
+                lock (_connection)
+                {
+                    _connection.Close();
+                    _connection.Dispose();
+                }
+                _connection = null;
+            }
+
+            if (_connectionFactory != null)
+            {
+                lock (_connectionFactory)
+                {
+                    _connectionFactory = null;
+                }
+            }
+        }
+
+        private bool IsConnected()
+        {
+            if (_connectionFactory == null) return false;
+            if (_connection == null) return false;
+            if (_model == null) return false;
+            return _model.IsOpen;
+        }
+    }
+}
