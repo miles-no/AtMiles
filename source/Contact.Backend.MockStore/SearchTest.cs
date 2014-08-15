4922dd974ca416472e11f6e60eda256703d15291
sedvardsen <sedvardsen@gmail.com>
2014-08-15 14:32:26 +0200

    Looking at read store

---
+++ source/Contact.Backend.MockStore/SearchTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Raven.Abstractions.Indexing;
+using Raven.Client;
+
+namespace Contact.Backend.MockStore
+{
+    public class SearchTest
+    {
+        
+        public class SearchMaterial
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string[] Tags { get; set; }
+        }
+
+        private const string SearchIndexName = "SearchMaterialByName";
+
+        public SearchTest()
+        {
+            Store = MockStore.DocumentStore;
+        }
+
+        public IDocumentStore Store { get; set; }
+
+        public List<SearchMaterial> Search(string searchString)
+        {
+            List<SearchMaterial> results;
+            using (var session = Store.OpenSession())
+            {
+                results = session.Query<SearchMaterial>(SearchIndexName)
+                    .Search(x => x.FirstName, searchString, 10)
+                    .Search(x => x.LastName, searchString, 10)
+                    .Search(x => x.Tags, searchString)
+                    .ToList();
+
+            }
+            return results;
+        }
+
+        public void Setup()
+        {
+            
+            
+            Store.DatabaseCommands.PutIndex(SearchIndexName, new IndexDefinition
+            {
+                Map = "from user in docs.SearchMaterials select new { user.FirstName, user.LastName, user.Tags }",
+                Indexes = { { "FirstName", FieldIndexing.Analyzed },{"LastName", FieldIndexing.Analyzed},{"Tags", FieldIndexing.Analyzed} }
+            }, true);
+
+            var test1 = new SearchMaterial {Id = "SearchMaterials/1", FirstName = "Banana", LastName = "Split", Tags = new []{"Dessert","Old school"}};
+            var test2 = new SearchMaterial {Id = "SearchMaterials/2", FirstName = "Jack", LastName = "Black", Tags = new[] { "Actor", "Rock school","Banana fan" } };
+
+            using (var session = Store.OpenSession())
+            {
+                session.Store(test1);
+                session.Store(test2);
+                session.SaveChanges();
+                // just be sure its indexed
+                var results =
+                    session.Query<SearchMaterial>(SearchIndexName)
+                        .Customize(x => x.WaitForNonStaleResults(TimeSpan.FromSeconds(2)))
+                        .ToList();
+            }
+
+        }
+    }
+}
---
+++ source/Contact.ReadStore.Test/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Contact.Backend.MockStore;
+
+namespace Contact.ReadStore.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new SearchTest();
+            //test.Setup();
+            //Search(test,"Jack");
+            //Search(test, "Banana");
+            //Search(test, "Dessert Jack");
+
+            var fetcher = new EventFetcher();
+            fetcher.Subscribe(async (e) =>
+            {
+                lock("updateInSeqence")
+                {
+                    Console.WriteLine(e.Event.EventType);
+                    var json = Encoding.UTF8.GetString(e.Event.Data);
+                }
+
+            }, new[] { "EmployeeAdded" });
+
+            Console.ReadLine();
+        }
+
+
+        public class PersonSearchModel
+        {
+            public string CompanyId { get; set; }
+            public string OfficeId { get; set; }
+            public string GlobalId { get; set; }
+            public string Name { get; set; }
+
+            public readonly string FirstName;
+            public readonly string MiddleName;
+            public readonly string LastName;
+            public readonly DateTime DateOfBirth;
+            public readonly string JobTitle;
+            public readonly string PhoneNumber;
+            public readonly string Email;
+            public readonly string Thumb;
+        }
+
+        public class EmployeeAddedModel
+        {
+            public string CompanyId { get; set; }
+            public string OfficeId { get; set; }
+            public string GlobalId { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string JobTitle { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public Picture Photo { get; set; }
+        }
+
+        public class Picture
+        {
+            public string Title { get; set; }
+            public string Extension { get; set; }
+            public byte[] Content { get; set; }
+        }
+
+        public static void Search(SearchTest search, string searchString)
+        {
+            Print("\n\nSearching for \"" + searchString + "\"");
+            var res = search.Search(searchString);
+            Print(res);
+
+        }
+        public static void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void Print(List<SearchTest.SearchMaterial> materials)
+        {
+            Print("Results:\n");
+            foreach (var searchMaterial in materials)
+            {
+                Print(searchMaterial.FirstName + " " + searchMaterial.LastName + " (" + string.Join(",",searchMaterial.Tags) + ")");
+            }
+        }
+    }
+}
