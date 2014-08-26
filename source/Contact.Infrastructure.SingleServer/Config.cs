a8b650de805ba4a72275c33d7350682b2cac4e56
Roy Veshovda <roy@veshovda.no>
2014-08-26 12:31:41 +0200

    Separate project for Background process

---
+++ source/Contact.Infrastructure.SingleServer/Config.cs
@@ -0,0 +1,14 @@
+﻿namespace Contact.Infrastructure.SingleServer
+{
+    public class Config
+    {
+        public string EventServerHost { get; set; }
+        public string EventServerSUsername { get; set; }
+        public string EventServerSPassword { get; set; }
+        public string RabbitMqHost { get; set; }
+        public string RabbitMqUsername { get; set; }
+        public string RabbitMqPassword { get; set; }
+        public string RabbitMqQueueName { get; set; }
+        public string CvPartnerToken { get; set; }
+    }
+}
---
+++ source/Contact.Infrastructure.SingleServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Contact.Domain.Aggregates;
+using Contact.Domain.CommandHandlers;
+using Contact.Import.CvPartner.CvPartner;
+using Contact.Infrastructure.SingleServer.Properties;
+using Newtonsoft.Json;
+
+namespace Contact.Infrastructure.SingleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LongRunningProcess commandWorker = null;
+
+            var configFilename = Settings.Default.ConfigFile;
+            var config = GetConfig(configFilename);
+            commandWorker = StartCommandHandler(config);
+
+            Console.WriteLine("Press enter to finish");
+            Console.ReadLine();
+
+            commandWorker.Stop();
+        }
+
+        private static Config GetConfig(string configFilename)
+        {
+            if (!File.Exists(configFilename)) throw new FileNotFoundException();
+
+            var raw = File.ReadAllText(configFilename);
+            var config = JsonConvert.DeserializeObject<Config>(raw);
+
+            return config;
+        }
+
+        private static LongRunningProcess StartCommandHandler(Config config)
+        {
+            var companyRepository = new EventStoreRepository<Company>(config.EventServerHost, null, config.EventServerSUsername, config.EventServerSPassword);
+            var employeeRepository = new EventStoreRepository<Employee>(config.EventServerHost, null, config.EventServerSUsername, config.EventServerSPassword);
+            var globalRepository = new EventStoreRepository<Global>(config.EventServerHost, null, config.EventServerSUsername, config.EventServerSPassword);
+            var commandSessionRepository = new EventStoreRepository<CommandSession>(config.EventServerHost, null, config.EventServerSUsername, config.EventServerSPassword);
+
+            var importer = new ImportMiles(config.CvPartnerToken);
+            var cmdHandler = MainCommandHandlerFactory.Initialize(companyRepository, employeeRepository, globalRepository, importer);
+            var cmdReceiver = new RabbitMqCommandHandler(cmdHandler, commandSessionRepository);
+
+            var logger = new ConsoleLogger();
+            var worker = new QueueWorker(config.RabbitMqHost, config.RabbitMqUsername, config.RabbitMqPassword, config.RabbitMqQueueName, logger, cmdReceiver.MessageHandler);
+
+            worker.Start();
+
+            Console.WriteLine("Command-worker Started");
+            return worker;
+        }
+    }
+}
