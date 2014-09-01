fefc82431dd252ebe23cf98c570762f9eea9d9d0
Roy Veshovda <roy@veshovda.no>
2014-09-01 14:51:38 +0200

    Cleanup and better method for creating indexes in RavenDB

---
+++ source/Contact.ReadStore.Test/EventStoreGlobalPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace Contact.ReadStore
+{
+    public class EventStoreGlobalPosition
+    {
+        public string Id { get; set; }
+        public long PreparePosition { get; set; }
+        public long CommitPosition { get; set; }
+    }
+}
---
+++ source/Contact.ReadStore.Test/PositionSaver.cs
@@ -0,0 +1,34 @@
+﻿using Raven.Client;
+
+namespace Contact.ReadStore
+{
+    public class PositionSaver
+    {
+        private const string PositionId = "global/POSITION";
+        private readonly IDocumentStore _documentStore;
+
+        public PositionSaver(IDocumentStore documentStore)
+        {
+            _documentStore = documentStore;
+        }
+
+        public EventStoreGlobalPosition GetLatestSavedPosition()
+        {
+            using (var session = _documentStore.OpenSession())
+            {
+                var position = session.Load<EventStoreGlobalPosition>(PositionId);
+                return position;
+            }
+        }
+
+        public void SaveLatestPosition(EventStoreGlobalPosition position)
+        {
+            position.Id = PositionId;
+            using (var session = _documentStore.OpenSession())
+            {
+                session.Store(position);
+                session.SaveChanges();
+            }
+        }
+    }
+}
