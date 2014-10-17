cc25b03a20fe8f21872dea93f4d9c4c49cf526ec
Roy Veshovda <roy@veshovda.no>
2014-10-17 09:48:13 +0200

    Bugfix

--- source/Backend/Infrastructure/Configuration/Config.cs
+++ source/Backend/Infrastructure/Configuration/Config.cs
@@ -2,21 +2,21 @@
 {
     public class Config
     {
-        public string CompanyId;
-        public string StatusEndpointUrl;
-        public string EventServerHost;
-        public string EventServerUsername;
-        public string EventServerPassword;
-        public string RabbitMqHost;
-        public string RabbitMqUsername;
-        public string RabbitMqPassword;
-        public bool RabbitMqUseSsl;
-        public string RabbitMqCommandQueueName;
-        public string RabbitMqCommandExchangeName;
-        public string CvPartnerToken;
-        public string RavenDbUrl;
-        public string Auth0Issuer;
-        public string Auth0Audience;
-        public string Auth0Secret;
+        public string CompanyId { get; set; }
+        public string StatusEndpointUrl { get; set; }
+        public string EventServerHost { get; set; }
+        public string EventServerUsername { get; set; }
+        public string EventServerPassword { get; set; }
+        public string RabbitMqHost { get; set; }
+        public string RabbitMqUsername { get; set; }
+        public string RabbitMqPassword { get; set; }
+        public bool RabbitMqUseSsl { get; set; }
+        public string RabbitMqCommandQueueName { get; set; }
+        public string RabbitMqCommandExchangeName { get; set; }
+        public string CvPartnerToken { get; set; }
+        public string RavenDbUrl { get; set; }
+        public string Auth0Issuer { get; set; }
+        public string Auth0Audience { get; set; }
+        public string Auth0Secret { get; set; }
     }
 }
