7520c12ddf81bcd49ae98ecd3df02068ffe92631
sedvardsen <sedvardsen@gmail.com>
2014-10-20 22:57:15 +0200

    refactored web app. Removed visual studio stuff. Use bower for dependencies (added bower library to git - so new users wont get stuck right away and to keep stuff easy with azure)

--- source/Contact.Web/config.rb
+++ source/Contact.Web/config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets"
-sass_dir = "public/stylesheets/sass"
+sass_dir = "public/stylesheets"
 images_dir = "public/images"
 javascripts_dir = "public/javascripts"
 
