997b37508937d932dd88f1d773d95ad48804f1fa
Roy Veshovda <roy@veshovda.no>
2014-10-31 06:27:45 +0100

    Started API-doc

--- source/Backend/Api/Controllers/RootController.cs
+++ source/Backend/Api/Controllers/RootController.cs
@@ -9,6 +9,7 @@ namespace no.miles.at.Backend.Api.Controllers
         [Route("")]
         public HttpResponseMessage Get()
         {
+        	//TODO: Extend this controller to return different sections based on the user is signed in or not.
             const string res = "root";
             return Request.CreateResponse(HttpStatusCode.OK, res);
         }
