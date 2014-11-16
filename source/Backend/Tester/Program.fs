c015e1b1219b78f585af1fdef2e99147e582b29c
Roy Veshovda <roy@veshovda.no>
2014-11-16 23:30:57 +0100

    Migrated to F# test-app

--- source/Backend/Tester/Program.fs
+++ source/Backend/Tester/Program.fs
@@ -1,6 +1,11 @@
 ﻿open System
-//open no.miles.at.Backend.Infrastructure
-//open WorkerService
+open no.miles.at.Backend.Domain
+open no.miles.at.Backend.Domain.Commands
+open no.miles.at.Backend.Domain.ValueTypes
+open no.miles.at.Backend.Domain.Services
+open no.miles.at.Backend.Infrastructure
+open no.miles.at.Backend.Infrastructure.Configuration
+open WorkerService
 
 let printPoster workerRunning (message: string) = 
     Console.Clear()
@@ -41,41 +46,65 @@ let stopWorker (worker:WorkerService.WorkerProcess) running =
         worker.Stop()
         (false, true, "Stopped worker")
 
-let seedData running =
-    //TODO: Implement
+let importFromCvPartner (config: Config)  (logger:ConsoleLogger) =
+    
+    None
+
+let seedData running (logger : ILog) (config:Config) =
+    let companyId = config.CompanyId
+    let companyName = "Miles"
+    let initCorrelationId = "INIT SEED"
+    let systemAsPerson = new Person(Constants.SystemUserId, Constants.SystemUserId)
+    let admin1 = new SimpleUserInfo(IdService.CreateNewId(), "Roy", "", "Veshovda", new Login(Constants.GoogleIdProvider, "roy.veshovda@miles.no"))
+    let admin2 = new SimpleUserInfo(IdService.CreateNewId(), "Stian", "", "Edvardsen", new Login(Constants.GoogleIdProvider, "stian.edvardsen@miles.no"))
+    let admins = [|admin1;admin2|]
+
+    let seedCommand = new AddNewCompanyToSystem(companyId, companyName, admins, DateTime.UtcNow, systemAsPerson, initCorrelationId, Constants.IgnoreVersion)
+    use commandSender = new RabbitMqCommandSender(config.RabbitMqHost, config.RabbitMqUsername, config.RabbitMqPassword, config.RabbitMqCommandExchangeName, config.RabbitMqUseSsl, logger)
+    commandSender.Send seedCommand
     (running, true, "Seeded data")
 
-let importCvPartner running =
-    //TODO: Implement
+let importCvPartner running (logger : ILog) (config: Config)=
+    let companyId = config.CompanyId
+    let correlationId = IdService.CreateNewId()
+    let systemAsPerson = new Person(Constants.SystemUserId, Constants.SystemUserId)
+    let importCommand = new ImportDataFromCvPartner(companyId, DateTime.UtcNow, systemAsPerson, correlationId, Constants.IgnoreVersion)    
+    use commandSender = new RabbitMqCommandSender(config.RabbitMqHost, config.RabbitMqUsername, config.RabbitMqPassword, config.RabbitMqCommandExchangeName, config.RabbitMqUseSsl, logger)
+    commandSender.Send importCommand
     (running, true, "Imported from CV-partner")
 
-let importAuth0 running =
-    //TODO: Implement
+let importAuth0 running (logger : ILog) (config:Config) =
+    let companyId = config.CompanyId
+    let correlationId = IdService.CreateNewId()
+    let systemAsPerson = new Person(Constants.SystemUserId, Constants.SystemUserId)
+    let importCommand = new EnrichFromAuth0(companyId, DateTime.UtcNow, systemAsPerson, correlationId, Constants.IgnoreVersion)    
+    use commandSender = new RabbitMqCommandSender(config.RabbitMqHost, config.RabbitMqUsername, config.RabbitMqPassword, config.RabbitMqCommandExchangeName, config.RabbitMqUseSsl, logger)
+    commandSender.Send importCommand
     (running, true, "Imported from Auth0")
 
-let resolveAction worker running key = 
+let resolveAction config worker logger running key = 
     match key with
     | ConsoleKey.D1 -> startWorker worker running
     | ConsoleKey.NumPad1 -> startWorker worker running     
     | ConsoleKey.D2 -> stopWorker worker running
     | ConsoleKey.NumPad2 -> stopWorker worker running
-    | ConsoleKey.D3 -> seedData running
-    | ConsoleKey.NumPad3 -> seedData running
-    | ConsoleKey.D4 -> importCvPartner running
-    | ConsoleKey.NumPad4 -> importCvPartner running
-    | ConsoleKey.D5 -> importAuth0 running
-    | ConsoleKey.NumPad5 -> importAuth0 running
+    | ConsoleKey.D3 -> seedData running logger config
+    | ConsoleKey.NumPad3 -> seedData running logger config
+    | ConsoleKey.D4 -> importCvPartner running logger config
+    | ConsoleKey.NumPad4 -> importCvPartner running logger config
+    | ConsoleKey.D5 -> importAuth0 running logger config
+    | ConsoleKey.NumPad5 -> importAuth0 running logger config
     | ConsoleKey.Q ->
         (running, false, "Quit")
     | _ ->
         (running, true, "Unknown command")
 
-let rec loop (worker:WorkerService.WorkerProcess) running message =
+let rec loop config (worker:WorkerService.WorkerProcess) logger running message =
     printPoster running message
     let key = Console.ReadKey true
-    let (r, l, mes) = resolveAction worker running key.Key
+    let (r, l, mes) = resolveAction config worker logger running key.Key
     match l with
-    | true -> loop worker r mes
+    | true -> loop config worker logger r mes
     | false ->
         Console.WriteLine "Stopping..."
         worker.Stop()
@@ -84,7 +113,8 @@ let rec loop (worker:WorkerService.WorkerProcess) running message =
 [<EntryPoint>]
 let main argv = 
     argv |> ignore
-    let logger = new no.miles.at.Backend.Infrastructure.ConsoleLogger();
-    let worker = new WorkerService.WorkerProcess(logger);
-    loop worker false "" |> ignore
+    let config = Configuration.ConfigManager.GetConfigUsingDefaultConfigFile()
+    let logger = new ConsoleLogger();
+    let worker = new WorkerProcess(logger);
+    loop config worker logger false "" |> ignore
     0 // return an integer exit code
